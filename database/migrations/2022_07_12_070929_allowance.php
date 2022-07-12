df0f88084b36d9e3fea422506a98a22d7ad5af69
kiran thapa <thapakiran910@gmail.com>
2022-07-12 13:16:55 +0545

    allowance migration

---
+++ database/migrations/2022_07_12_070929_allowance.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(){
+schema::create("allowance",function(Blueprint $table){
+    $table->id();
+    $table->string(user_id);
+    $table->string(allowance_name);
+    $table->string(allowance_details);
+    $table->string(allowance_type);
+    $table->string(amount);
+    $table->string(total);
+    $table->string(issued_salary_id);
+    $table->string(issue_date);
+    $table->string(issue_date);
+    $table->string(created_at);
+    $table->string(updated_at);
+
+    $table->timestamps();
+    
+});
+}
+
+public function down(){
+
+}
+};
