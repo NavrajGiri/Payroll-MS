893b39c8e6ae6ca9370c24bc6bc05c627fd8e971
Prakash Timilsina <pracasify@gmail.com>
2022-07-13 13:42:43 +0545

    somethings

--- app/Models/Rank.php
+++ app/Models/Rank.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class issue_salary extends Model
+class Rank extends Model
 {
     use HasFactory;
-    protected $table = "issue_salary";
-    protected $fillable = ["user_id","date","note","amount","issued_by"];
+    protected $table = "ranks";
+    protected $fillable = ["name"];
 }
