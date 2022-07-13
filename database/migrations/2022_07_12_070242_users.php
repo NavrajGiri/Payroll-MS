d99579ffa9fd18b46721dcef66e48f13774776fd
Prakash Timilsina <pracasify@gmail.com>
2022-07-13 13:27:53 +0545

    fixes

--- database/migrations/2022_07_12_070242_users.php
+++ database/migrations/2022_07_12_070242_users.php
@@ -8,8 +8,7 @@ return new class extends Migration
 {
 
     public function up(){
-        Schema::create("users",function(Blueprint $table){
-            $table->id();
+        Schema::table("users",function(Blueprint $table){
             $table->string('computer_no');
             $table->string('rank');
             $table->string('date_of_birth');
@@ -25,9 +24,6 @@ return new class extends Migration
             $table->string('grade_id');
             $table->string('nagrik_lagani_kosh_no');
             $table->string('type');
-
-            $table->timestamps();
-
         });
     }
 
--- database/migrations/2022_07_12_070929_allowance.php
+++ database/migrations/2022_07_12_070929_allowance.php
@@ -7,26 +7,21 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(){
-schema::create("allowance",function(Blueprint $table){
-    $table->id();
-    $table->string(user_id);
-    $table->string(allowance_name);
-    $table->string(allowance_details);
-    $table->string(allowance_type);
-    $table->string(amount);
-    $table->string(total);
-    $table->string(issued_salary_id);
-    $table->string(issue_date);
-    $table->string(issue_date);
-    $table->string(created_at);
-    $table->string(updated_at);
+        Schema::create("allowance",function(Blueprint $table){
+            $table->id();
+            $table->string('user_id');
+            $table->string('allowance_name');
+            $table->string('allowance_details');
+            $table->string('allowance_type');
+            $table->string('amount');
+            $table->string('total');
+            $table->string('issued_salary_id');
+            $table->dateTime('issue_date');
+            $table->timestamps();
+        });
+    }
 
-    $table->timestamps();
+    public function down(){
 
-});
-}
-
-public function down(){
-
-}
+    }
 };
