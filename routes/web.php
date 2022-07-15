d87a2bd572b201b485dcf3c3fdc6120a442798c2
navaraj <navaraj@gmail.com>
2022-07-15 11:49:23 +0545

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
-    //  admin
+
+use Illuminate\Support\Facades\Route;
+use TijsVerkoyen\CssToInlineStyles\Css\Rule\Rule;
+           //  admin
 use App\Http\Livewire\AdminAllowanceAddLivewire;
 use App\Http\Livewire\AdminAllowanceLivewire;
 use App\Http\Livewire\AdminBillEditLivewire;
@@ -10,9 +13,6 @@ use App\Http\Livewire\AdminRankLivewire;
 use App\Http\Livewire\AdminUserLivewire;
 use App\Http\Livewire\AdminUserEditLivewire;
 use App\Http\Livewire\AdminUserAddLivewire;
-
-use Illuminate\Support\Facades\Route;
-use TijsVerkoyen\CssToInlineStyles\Css\Rule\Rule;
 //    user
 use App\Http\Livewire\UserDashboardLivewire;
 use App\Http\Livewire\UserPaymentLivewire;
@@ -20,17 +20,13 @@ use App\Http\Livewire\UserBillLivewire;
 use App\Http\Livewire\UserAllowance;
 use App\Http\Livewire\UserBillAddLivewire;
 use App\Http\Livewire\UserBillEditLivewire;
-
-
     //    user
-
 Route::get('/dashboard',UserDashboardLivewire::class);
 Route::get('/payments',UserPaymentLivewire::class);
 Route::get('/bill',UserBillLivewire::class);
 Route::get('bill/add',UserBillAddLivewire::class);
 Route::get('bil/edit',UserBillEditLivewire::class);
 Route::get('allowance/',UserAllowance::class);
-
 //    admin
 Route::get('user/',AdminUserLivewire::class);
 Route::get('user/edit',AdminUserEditLivewire::class);
@@ -62,5 +58,4 @@ Route::middleware([
     })->name('dashboard');
 });
 
-
-// user
+//user
