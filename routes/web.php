807c3bfe98ead8cff7955e8c897766d97a95d8a3
Durga Timilsina <timilsinadurga89@gmail.com>
2022-07-15 12:34:03 +0545

    test

--- routes/web.php
+++ routes/web.php
@@ -13,70 +13,13 @@ use App\Http\Livewire\AdminUserAddLivewire;
 
 use Illuminate\Support\Facades\Route;
 use TijsVerkoyen\CssToInlineStyles\Css\Rule\Rule;
-<<<<<<< HEAD
-use App\Http\Livewire\UserDashboardLivewire;
-use App\Http\Livewire\UserPaymentLivewire;
-use App\Http\Livewire\BillsLivewire;
-use App\Http\Livewire\BillsAddLivewire;
-use App\Http\Livewire\BillsEditLivewire;
-use App\Http\Livewire\AllowanceLivewire;
 
-
-
-use App\Http\Livewire\AdminUserLivewire;
-use App\Http\Livewire\AdminUserEditLivewire;
-use App\Http\Livewire\AdminUserAddLivewire;
-use App\Http\Livewire\AdminRankLivewire;
-use App\Http\Livewire\AdminRankEditLivewire;
-use App\Http\Livewire\AdminRankAddLivewire;
-use App\Http\Livewire\AdminGradesLivewire;
-use App\Http\Livewire\AdminBillsLivewire;
-use App\Http\Livewire\AdminBillsEditLivewire;
-
-use App\Http\Livewire\AdminAllowanceLivewire;
-use App\Http\Livewire\AdminAllowanceAddLivewire;
-use App\Http\Livewire\AdminAllowanceEditLivewire;
-use App\Http\Livewire\IssuedSalaryLivewire;
-
-
-
-
-
-
-Route::get('/dashboard',UserDashboardLivewire::class);
-Route::get('/payments',UserPaymentLivewire::class);
-Route::get('/bill',UserBillLivewire::class);
-Route::get('/bill_add',BillAddLivewire::class);
-Route::get('/bill_edit',BillsEditLivewire::class);
-Route::get('/allowance',AllowanceLivewire::class);
-
-
-Route::get('/adminuser',AdminUserLivewire::class);
-Route::get('/adminuseredit',AdminUserEditLivewire::class);
-Route::get('/adminuseradd',AdminUserAddLivewire::class);
-Route::get('/adminrank',AdminRAnkLivewire::class);
-Route::get('/adminrankedit',AdminRankEditLivewire::class);
-Route::get('/adminrankadd',AdminRankAddLivewire::class);
-
-
-Route::get('/admingrades',AdminGradeLivewire::class);
-Route::get('/adminbill',AdminBillLivewire::class);
-Route::get('/adminbilledit',AdminBillEditditLivewire::class);
-Route::get('/adminallowance',AdminAllowanceLivewire::class);
-Route::get('/adminallowanceadd',AdminAllowanceAddLivewire::class);
-Route::get('/adminallowanceedit',AdminAllowanceEditLivewire::class);
-Route::get('/adminissuedsalary',AdminIssuedSalaryLivewire::class);
-
-
-=======
-//    user
 use App\Http\Livewire\UserDashboardLivewire;
 use App\Http\Livewire\UserPaymentLivewire;
 use App\Http\Livewire\UserBillLivewire;
 use App\Http\Livewire\UserAllowance;
 use App\Http\Livewire\UserBillAddLivewire;
 use App\Http\Livewire\UserBillEditLivewire;
->>>>>>> 075d2d8b350edcc4cf18fae559757be033abe958
 
 
     //    user
