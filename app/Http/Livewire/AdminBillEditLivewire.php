917467485e12716c07f165f126719a328f0928ba
navaraj <navaraj@gmail.com>
2022-07-19 12:32:54 +0545

    update

--- app/Http/Livewire/AdminBillEditLivewire.php
+++ app/Http/Livewire/AdminBillEditLivewire.php
@@ -7,7 +7,6 @@ use Livewire\Component;
 class AdminBillEditLivewire extends Component
 {
 
-<<<<<<< HEAD
     public $bill_id,$bill_no,$user_id,$product_name,$product_details,$quantity,$amount,$issued_salary_id,$total_amount;
     public function mount($bill_id){
         $this->bill_id = $bill_id;
@@ -35,36 +34,7 @@ class AdminBillEditLivewire extends Component
     $bill->issued_salary_id = $bill ->issued_salary_id;
     $bill->total_amount = $bill->total_amount;
     $bill->save();
-    return redirect(route('admin.bill'));
-=======
-    public $id,$bill_no,$user_id,$product_name,$product_details,$quantity,$amount,$issued_salary_id,$total_amount;
-    public function mount($id){
-        $this->id = $bill_id;
-        $bill = Bill::find($id);
-        $this->bill_no = $bill->$bill_no;
-        $this->user_id = $bill->$user_id;
-        $this->product_name = $bill->$product_name;
-        $this->product_details = $bill->$product_details;
-        $this->quantity = $bill->$quantity;
-        $this->amount = $bill->$amount;
-        $this->issued_salary_id = $bill->$issued_salary_id;
-        $this->total_amount = $bill->$total_amount;
 
-    }
-
-    public function save(){
-$bill = Bill::find($this->id);
-$this->bill_no = $bill->$bill_no;
-$this->user_id = $bill->$user_id;
-$this->product_name = $bill->$product_name;
-$this->product_details = $bill->$product_details;
-$this->quantity = $bill->$quantity;
-$this->amount = $bill->$amount;
-$this->issued_salary_id = $bill->$issued_salary_id;
-$this->total_amount = $bill->$total_amount;
-$bill->save();
-return redirect(route('admin.bill'));
->>>>>>> c32765c5a353615341c0492cc9db4100aae4e865
 
         }
 
@@ -73,5 +43,6 @@ return redirect(route('admin.bill'));
             return view('livewire.admin-bill-edit-livewire');
         }
     }
+
 
 
