327d51edc42f865b900fe158af5a58ff9dca8c0e
Durga Timilsina <timilsinadurga89@gmail.com>
2022-07-21 09:41:26 +0545

    chng

--- resources/views/livewire/admin-user-add-livewire.blade.php
+++ resources/views/livewire/admin-user-add-livewire.blade.php
@@ -135,12 +135,7 @@
         <label class="block text-gray-700 text-sm font-bold mb-2">
           Grade Id
         </label>
-        <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" wire:model='rank'>
-            <option>Please select Rank</option>
-            @foreach ($all_grades as $grade)
-                <option value="{{$grades->id}}">{{$grades->name}}</option>
-            @endforeach
-        </select>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" wire:model="salary_amount" id="salary_amount" type="text" placeholder="salary amount">
 
       </div><div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" >
